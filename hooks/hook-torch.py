b47e7a9269b080706ffd9bdd3f3732fb3ce7deb3
Baoqiang Ma <86932526+baoqiangmaUMCG@users.noreply.github.com>
2025-09-10 16:30:04 +0200

    Update hook-torch.py

--- hooks/hook-torch.py
+++ hooks/hook-torch.py
@@ -1,13 +1,37 @@
+import os, sys, glob
 from PyInstaller.utils.hooks import collect_submodules, collect_data_files, collect_dynamic_libs
-import sys
 
 hiddenimports = collect_submodules("torch")
-
-# Include all torch .py files
 datas = collect_data_files("torch", include_py_files=True)
+binaries = collect_dynamic_libs("torch")
 
-# On Linux, also include the .so libraries
 if sys.platform.startswith("linux"):
-    binaries = collect_dynamic_libs("torch")
-else:
-    binaries = []
+    cudnn_libs = []
+
+    # Get current conda/venv prefix
+    env_prefix = os.environ.get("CONDA_PREFIX", sys.prefix)
+
+    #cudnn_path = os.path.join(env_prefix, "lib", "python3.11", "site-packages", "nvidia", "cudnn", "lib")
+    
+    #import sys
+    pyver = f"python{sys.version_info.major}.{sys.version_info.minor}"
+    cudnn_path = os.path.join(env_prefix, "lib", pyver, "site-packages", "nvidia", "cudnn", "lib")
+    
+    if os.path.isdir(cudnn_path):
+        cudnn_libs.extend(glob.glob(os.path.join(cudnn_path, "libcudnn*.so*")))
+
+    for lib in cudnn_libs:
+        binaries.append((lib, "torch/lib"))
+
+        # Ensure symlink chain (.so ? .so.9 ? .so.9.1.0) is available
+        if ".so." in lib:
+            base = lib.split(".so.")[0] + ".so"
+            linkname = os.path.join(os.path.dirname(lib), os.path.basename(base))
+            if not os.path.exists(linkname):
+                try:
+                    os.symlink(os.path.basename(lib), linkname)
+                except FileExistsError:
+                    pass
+            binaries.append((linkname, "torch/lib"))
+
+print (binaries)
